6644e05134c4f64a5bb682cf1ab345b6d0bb3c62
lyukio <lyukio@hotmail.com>
2018-09-11 12:00:27 -0300

    starting Debug

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
+
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
@@ -52,4 +54,12 @@ function onError(error) {
         default:
             throw error;
     }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
